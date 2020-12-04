d19ec5576bc0b1c9f5b6a797442ab288d7fe9f43
proiacm <proiacm@gmail.com>
2020-12-04 12:35:32 -0700

    Add form to Signup

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Signup.css';
+import logo from '../Images/logo.png';
 
 const Signup = () => {
 
   return (
-    <div>
-      Signup
+    <div className='signup'>
+      <img className='logo' src={logo}/>
+              <div className='contact'>
+                <input type="text" id="name" name="name" placeholder="Name"/>
+                <input type="text" id="email" name="email" placeholder="Email"/>
+                <textarea id="subject" name="subject" placeholder="Tell us about you!" />
+                <input type="submit" value="Send"/>
+            </div>
     </div>
   )
 }
