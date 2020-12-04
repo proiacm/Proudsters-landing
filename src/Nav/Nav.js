c2756ad4e0e2517e1bb1c2db21eb6acc0120df81
proiacm <proiacm@gmail.com>
2020-12-04 11:56:55 -0700

    Add Nav

---
+++ src/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-scroll';
+
+const Nav = () => {
+
+  return (
+      <div className='nav'>
+          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="landing" spy={true} smooth={true}><u>Home</u></Link></p>
+          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>About</u></Link></p>
+      </div>
+  )
+}
+
+export default Nav;
