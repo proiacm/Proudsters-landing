cae2c70274bdef6b4fa9444811813a6d619d32a4
proiacm <proiacm@gmail.com>
2020-12-04 13:30:40 -0700

    Add title and styling

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './Signup.css';
-import logo from '../Images/logo.png';
 
 const Signup = () => {
 
   return (
-    <div className='signup'>
-      <img className='logo' src={logo}/>
+    <div className='signup' id='signup'>
               <div className='contact'>
+                <h1>Contact Us</h1>
                 <input type="text" id="name" name="name" placeholder="Name"/>
                 <input type="text" id="email" name="email" placeholder="Email"/>
                 <textarea id="subject" name="subject" placeholder="Tell us about you!" />
