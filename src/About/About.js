e3741a33defcb07f37d7d35f20eaa32e661ba05a
proiacm <proiacm@gmail.com>
2020-12-04 23:27:31 -0700

    Restyle About

--- src/About/About.js
+++ src/About/About.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import './About.css';
-import logo from '../Images/logo.png';
+// import logo from '../Images/logo.png';
 
 const About = () => {
 
   return (
     <div className='about' id='about'>
-          <img className='logo' src={logo}/>
-          <p className='about-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
-            pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. 
-            Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, 
-            in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent
-            per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut 
-            vestibulum. Maecenas ipsum lacus, lacinia quis posuere ut, pulvinar vitae dolor.
-            Integer eu nibh at nisi ullamcorper sagittis id vel leo. Integer feugiat 
-            faucibus libero, at maximus nisl suscipit posuere. Morbi nec enim nunc. 
-            Phasellus bibendum turpis ut ipsum egestas, sed sollicitudin elit convallis. 
-            Cras pharetra mi tristique sapien vestibulum lobortis. Nam eget bibendum metus, 
-            non dictum mauris. Nulla at tellus sagittis, viverra est a, bibendum metus.
+          <section className="text">
+            <h3 className='about-title'>Coming Soon</h3>
+            <h1 className='about-name'><span className="fontawesome-star star"></span> <span>Proudsters</span> <span className="fontawesome-star star"></span></h1>
+          </section>
+          <p className='about-desc'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+          pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. 
+          Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, 
+          in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent
+          per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut 
+          vestibulum. Maecenas ipsum lacus, lacinia quis posuere ut, pulvinar vitae dolor.
+          Integer eu nibh at nisi ullamcorper sagittis id vel leo. Integer feugiat 
+          faucibus libero, at maximus nisl suscipit posuere. Morbi nec enim nunc. 
+          Phasellus bibendum turpis ut ipsum egestas, sed sollicitudin elit convallis. 
+          Cras pharetra mi tristique sapien vestibulum lobortis. Nam eget bibendum metus, 
+          non dictum mauris. Nulla at tellus sagittis, viverra est a, bibendum metus.
           </p>
-          <h1>Maybe some picutres?</h1>
     </div>
   )
 }
