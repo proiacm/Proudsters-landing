68e7a1e2de56202acea0cfa19df18c510175c028
proiacm <proiacm@gmail.com>
2020-12-04 23:27:44 -0700

    Move Nav to side

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => {
 
   return (
       <div className='nav'>
-          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>Home</u></Link></p> |
+          <p className='nav-li'><Link style={{ cursor: "pointer"}} to="about" spy={true} smooth={true}><u>Home</u></Link></p>
           <p className='nav-li'><Link style={{ cursor: "pointer"}} to="signup" spy={true} smooth={true}><u>Join</u></Link></p>
       </div>
   )
